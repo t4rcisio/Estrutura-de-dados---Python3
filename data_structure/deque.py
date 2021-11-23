f42afb901ed61afec1b6595a05d50e3458ea0908
t4rcisio <tarcisiobatistaprates@gmail.com>
2021-11-23 00:37:32 -0300

    deque

---
+++ data_structure/deque.py
@@ -0,0 +1,45 @@
+class Deque:
+    def __init__(self):
+        self.__deque = []
+        self.__deque_lenght = 0
+
+    def putTop(self, val):
+        self.__deque.append(val)
+        self.__deque_lenght +=1
+    
+    def putBotton(self, val):
+        self.__deque.insert(0,val)
+        self.__deque_lenght =+1
+    
+    def popTop(self):
+        self.__deque.pop()
+        self.__deque_lenght -=1
+    
+    def popBotton(self):
+        self.__deque.pop(0)
+        self.__deque_lenght -=1
+
+    def top(self):
+        return self.__deque[-1]
+    
+    def botton(self):
+        return self.__deque[0]
+    
+    def lenght(self):
+        return self.__deque_lenght
+    
+    def deque(self):
+        return self.__deque
+
+
+
+deque = Deque()
+
+for i in range(15):
+    deque.putTop(i)
+    deque.putBotton(-i)
+
+print(deque.deque())
+print(deque.top())
+print(deque.botton())
+
